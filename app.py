3654486604f8df1f8713d2b8d8d13ece8d11d0fc
Vustron <runa.michaeljoshua@dnsc.edu.ph>
2024-02-07 12:24:21 +0800

    Upload the whole backend server

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, request, redirect, render_template
+from flask_cors import CORS,cross_origin
+from flask import jsonify
+import pickle
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+cors = CORS(app)
+model = pickle.load(open('LinearRegressionModel.pkl','rb'))
+car = pd.read_csv('cleaned_car_data.csv')
+
+
+@app.route('/data', methods=['GET', 'POST'])
+@cross_origin()
+def index():
+    try:
+        companies = car['company'].unique()
+        car_models = car['name'].unique()
+        year = car['year'].unique()
+        fuel_type = car['fuel_type'].unique()
+    
+        data = {
+            "companies": [str(company) for company in companies.tolist()],
+            "car_models": [str(model) for model in car_models.tolist()],
+            "years": [str(year) for year in year.tolist()],
+            "fuel_types": [str(fuel) for fuel in fuel_type.tolist()]
+        }
+
+        return jsonify(data)
+    except Exception as e:
+        app.logger.error(f"An error occurred: {str(e)}")
+        return jsonify({"error": "Internal Server Error"}), 500
+
+@app.route('/predict',methods=['POST'])
+@cross_origin()
+def predict():
+
+    try:
+        data = request.json
+        companies = data.get('companies')
+        car_models = data.get('car_models')
+        years = data.get('years')
+        fuel_types = data.get('fuel_types')
+        kmTravelled = data.get('kmTravelled')
+
+        prediction=model.predict(pd.DataFrame(columns=['name', 'company', 'year', 'fuel_type', 'kms_driven',],
+                                data=np.array([car_models,companies,years,fuel_types,kmTravelled]).reshape(1, 5)))
+        
+        response = {
+            "prediction": np.round(prediction[0], 2).tolist()
+        }
+
+        # return str(np.round(prediction[0],2))
+        return jsonify(response)
+    except Exception as e:
+        app.logger.error(f"An error occurred: {str(e)}")
+        return jsonify({"error": "Internal Server Error"}), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
